e8a37942191c68493137379b33aeb80fca0ccbb1
saiful201 <118247523+saiful201@users.noreply.github.com>
2025-03-04 11:27:27 +0300

    Update angry.js

--- Priyansh/commands/angry.js
+++ Priyansh/commands/angry.js
@@ -12,7 +12,7 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if (event.body.indexOf("😤")==0 || (event.body.indexOf("😠")==0 || (event.body.indexOf("😡")==0 || (event.body.indexOf("😈")==0)))) {
+	if (event.body.indexOf("🤬")==0 || (event.body.indexOf("😈")==0 || (event.body.indexOf("👿")==0 || (event.body.indexOf("👿👿")==0)))) {
 		var msg = {
 				body: "",
 				attachment: fs.createReadStream(__dirname + `/noprefix/damimanus.mp3`)
